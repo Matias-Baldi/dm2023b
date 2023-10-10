0f2aabcdff2ef2d04512a32269ece10737f9291a
Matias-Baldi <baldi.matias.e1@gmail.com>
2023-10-10 22:10:00 +0000

    qw

--- src/workflow-1/631_FE_historia_2.r
+++ src/workflow-1/631_FE_historia_2.r
@@ -49,11 +49,14 @@ PARAM$RandomForest$num.trees <- 20
 PARAM$RandomForest$max.depth <- 4
 PARAM$RandomForest$min.node.size <- 1000
 PARAM$RandomForest$mtry <- 40
-PARAM$RandomForest$semilla <- 900019  # cambiar por la propia semilla
-
+PARAM$RandomForest$semilla <- 900019 # cambiar por la propia semilla
+PARAM$RandomForest$splitrule <- "hellinger"
+#PARAM$RandomForest$importance <- "permutation"
+PARAM$RandomForest$sample.fraction <- 0.8
+#PARAM$RandomForest$replace <- FALSE #Si es false el sample fraction cambia por 0.632, si es true prueba con 1.
 
 # varia de 0.0 a 2.0, si es 0.0 NO se activan
-PARAM$CanaritosAsesinos$ratio <- 0.0
+PARAM$CanaritosAsesinos$ratio <- 0.5
 # desvios estandar de la media, para el cutoff
 PARAM$CanaritosAsesinos$desvios <- 4.0
 # cambiar por la propia semilla
