d46d50b258015e2b56c5e911ff23a6d3016369b2
Matias-Baldi <baldi.matias.e1@gmail.com>
2023-10-13 23:10:18 +0000

    d

--- src/workflow-3/601_RUN_correr_workflow_04.r
+++ src/workflow-3/601_RUN_correr_workflow_04.r
@@ -10,11 +10,11 @@ options(error = function() {
 # corrida de cada paso del workflow
 
 # primeros pasos, relativamente rapidos
-source("~/dm2023b/src/workflow-inicial/611_CA_reparar_dataset_1.r")
-source("~/dm2023b/src/workflow-inicial/621_DR_corregir_drifting_1.r")
-source("~/dm2023b/src/workflow-inicial/631_FE_historia_1.r")
-source("~/dm2023b/src/workflow-inicial/641_TS_training_strategy_1.r")
+source("~/dm2023b/src/workflow-inicial/611_CA_reparar_dataset_4.r")
+source("~/dm2023b/src/workflow-inicial/621_DR_corregir_drifting_4.r")
+source("~/dm2023b/src/workflow-inicial/631_FE_historia_4.r")
+source("~/dm2023b/src/workflow-inicial/641_TS_training_strategy_4.r")
 
 # ultimos pasos, muy lentos
-source("~/dm2023b/src/workflow-inicial/651_HT_lightgbm_1.r")
-source("~/dm2023b/src/workflow-inicial/661_ZZ_final_1.r")
+source("~/dm2023b/src/workflow-inicial/651_HT_lightgbm_4.r")
+source("~/dm2023b/src/workflow-inicial/661_ZZ_final_4.r")
